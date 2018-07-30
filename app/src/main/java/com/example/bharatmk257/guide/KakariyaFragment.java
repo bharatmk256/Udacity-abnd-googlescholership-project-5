4ea3f5e4ee86115142862c8d684b9ffb7020e74c
bharatmk257 <bharatmk257@gmail.com>
2018-07-31 02:32:13 +0530

    need help to solve this not showing anything in app blank

--- app/src/main/java/com/example/bharatmk257/guide/KakariyaFragment.java
+++ app/src/main/java/com/example/bharatmk257/guide/KakariyaFragment.java
@@ -1,16 +1,12 @@
 package com.example.bharatmk257.guide;
 
-import android.content.res.Resources;
-import android.graphics.Rect;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
-import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RelativeLayout;
 
 import java.util.List;
 
@@ -18,17 +14,31 @@ public class KakariyaFragment extends Fragment {
 
     private RecyclerView recyclerView;
     private PlaceAdapter adapter;
-    private List<Places>placesList;
+    private List<Places> placesList;
 
+    @Nullable
     @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-//
-//        return inflater.inflate(R.layout.fragment_kakariya,container,false);
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+//View view = inflater.inflate(R.layout.fragment_kakariya,container,false);
+        return inflater.inflate(R.layout.fragment_kakariya, null);
     }
 
-    private void photos(){
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        initViews();
+        photos();
+    }
+
+    private void initViews() {
+        recyclerView = getActivity().findViewById(R.id.recyclerViewPlace);
+    }
+
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//    }
+
+    private void photos() {
         int[] covers = new int[]{
                 R.drawable.tajmahal,
                 R.drawable.tajmahal,
@@ -43,44 +53,42 @@ public class KakariyaFragment extends Fragment {
         };
 
         Places
-        a = new Places("tajmahal",covers[0],"Location");
+                a = new Places("tajmahal", covers[0], "Location");
         placesList.add(a);
-
-        a = new Places("chali",covers[1],"Loction");
+        a = new Places("chali", covers[1], "Loction");
         placesList.add(a);
-        a = new Places("whatsapp",covers[2],"Loction");
+        a = new Places("whatsapp", covers[2], "Loction");
         placesList.add(a);
-        a = new Places("samsung",covers[3],"Loction");
+        a = new Places("samsung", covers[3], "Loction");
         placesList.add(a);
-        a = new Places("gmail",covers[4],"Loction");
+        a = new Places("gmail", covers[4], "Loction");
         placesList.add(a);
-        a = new Places("motorola",covers[5],"Loction");
+        a = new Places("motorola", covers[5], "Loction");
         placesList.add(a);
-        a = new Places("phone",covers[6],"Loction");
+        a = new Places("phone", covers[6], "Loction");
         placesList.add(a);
-        a = new Places("photo",covers[7],"Loction");
+        a = new Places("photo", covers[7], "Loction");
         placesList.add(a);
-        a = new Places("sony",covers[8],"Loction");
+        a = new Places("sony", covers[8], "Loction");
         placesList.add(a);
-        a = new Places("gionee",covers[9],"Loction");
+        a = new Places("gionee", covers[9], "Loction");
         placesList.add(a);
 
         adapter.notifyDataSetChanged();
     }
-    public class GridSpacingItemDecoration extends RecyclerView.ItemDecoration {
-        private int spanCount;
-        private int spacing;
-        private boolean includeEdge;
-
-        public GridSpacingItemDecoration(int spanCount, int spacing, boolean includeEdge) {
-            this.spanCount = spanCount;
-            this.spacing = spacing;
-            this.includeEdge = includeEdge;
-        }
-    }
-    private int dpToPx(int dp) {
-        Resources r = getResources();
-        return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics()));
-    }
+//    public class GridSpacingItemDecoration extends RecyclerView.ItemDecoration {
+//        private int spanCount;
+//        private int spacing;
+//        private boolean includeEdge;
+//
+//        public GridSpacingItemDecoration(int spanCount, int spacing, boolean includeEdge) {
+//            this.spanCount = spanCount;
+//            this.spacing = spacing;
+//            this.includeEdge = includeEdge;
+//        }
+//    }
+//    private int dpToPx(int dp) {
+//        Resources r = getResources();
+//        return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics()));
+//    }
 }
-//}
